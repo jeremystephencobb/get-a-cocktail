cba881bf0817c951e9799b7063160170a4016427
Thejaydubbz <jeremycobb.on@gmail.com>
2020-11-18 18:50:54 -0500

    refactor to se prompt ui

--- src/Main.go
+++ src/Main.go
@@ -1,18 +1,23 @@
 package main
 
 import (
-	"bufio"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/manifoldco/promptui"
 	"io/ioutil"
 	"net/http"
-	"os"
 	"reflect"
-	// "strconv"
 	"strings"
 )
+
+type DrinkList struct {
+	Drinks []struct {
+		StrDrink      string `json:"strDrink"`
+		StrDrinkThumb string `json:"strDrinkThumb"`
+		IDDrink       string `json:"idDrink"`
+	} `json:"drinks"`
+}
 
 // Generated with: https://mholt.github.io/json-to-go/
 type Drinks struct {
@@ -29,48 +34,39 @@ type Drinks struct {
 		Instructions                string `json:"strInstructions"`
 		DrinkThumb                  string `json:"strDrinkThumb"`
 		Ingredient1                 string `json:"strIngredient1"`
-		Ingredient2                 string `json:"strIngredient2"`
-		Ingredient3                 string `json:"strIngredient3"`
-		Ingredient4                 string `json:"strIngredient4"`
-		Ingredient5                 string `json:"strIngredient5"`
-		Ingredient6                 string `json:"strIngredient6"`
-		Ingredient7                 string `json:"strIngredient7"`
-		Ingredient8                 string `json:"strIngredient8"`
-		Ingredient9                 string `json:"strIngredient9"`
-		Ingredient10                string `json:"strIngredient10"`
-		Ingredient11                string `json:"strIngredient11"`
-		Ingredient12                string `json:"strIngredient12"`
-		Ingredient13                string `json:"strIngredient13"`
-		Ingredient14                string `json:"strIngredient14"`
-		Ingredient15                string `json:"strIngredient15"`
 		Measure1                    string `json:"strMeasure1"`
+		Ingredient2                 string `json:"strIngredient2"`
 		Measure2                    string `json:"strMeasure2"`
+		Ingredient3                 string `json:"strIngredient3"`
 		Measure3                    string `json:"strMeasure3"`
+		Ingredient4                 string `json:"strIngredient4"`
 		Measure4                    string `json:"strMeasure4"`
+		Ingredient5                 string `json:"strIngredient5"`
 		Measure5                    string `json:"strMeasure5"`
+		Ingredient6                 string `json:"strIngredient6"`
 		Measure6                    string `json:"strMeasure6"`
+		Ingredient7                 string `json:"strIngredient7"`
 		Measure7                    string `json:"strMeasure7"`
+		Ingredient8                 string `json:"strIngredient8"`
 		Measure8                    string `json:"strMeasure8"`
+		Ingredient9                 string `json:"strIngredient9"`
 		Measure9                    string `json:"strMeasure9"`
+		Ingredient10                string `json:"strIngredient10"`
 		Measure10                   string `json:"strMeasure10"`
+		Ingredient11                string `json:"strIngredient11"`
 		Measure11                   string `json:"strMeasure11"`
+		Ingredient12                string `json:"strIngredient12"`
 		Measure12                   string `json:"strMeasure12"`
+		Ingredient13                string `json:"strIngredient13"`
 		Measure13                   string `json:"strMeasure13"`
+		Ingredient14                string `json:"strIngredient14"`
 		Measure14                   string `json:"strMeasure14"`
 		Measure15                   string `json:"strMeasure15"`
+		Ingredient15                string `json:"strIngredient15"`
 		StrCreativeCommonsConfirmed string `json:"strCreativeCommonsConfirmed"`
 		DateModified                string `json:"dateModified"`
 	} `json:"drinks"`
 }
-
-var BBlack string = "\033[1;30m"
-var BRed string = "\033[1;31m"
-var BGreen string = "\033[1;32m"
-var BYellow string = "\033[1;33m"
-var BBlue string = "\033[1;34m"
-var BPurple string = "\033[1;35m"
-var BCyan string = "\033[1;36m"
-var BWhite string = "\033[1;37m"
 
 func main() {
 	fmt.Println(string(BRed), "Welcome to the cocktail recipe command line tool. Type `quit` at any time to exit the program")
@@ -78,58 +74,75 @@ func main() {
 }
 
 func handleUserInput() {
-	var searchType string
-	var query string
-	reader := bufio.NewReader(os.Stdin)
+Main:
 	for {
-		fmt.Println(string(BWhite), "Search by: `name`")
-		searchType, _ = reader.ReadString('\n')
-		searchType = strings.Replace(searchType, "\n", "", -1)
-		if searchType != "quit" {
-			prompt := fmt.Sprintf("Search for a cocktail by %v\n", searchType)
-			fmt.Println(prompt)
-			query, _ = reader.ReadString('\n')
-			query = strings.Replace(query, "\n", "", -1)
+		searchTypeprompt := promptui.Select{
+			Label: "Select Day",
+			Items: []string{"name",
+				"ingredient name",
+				"random",
+				"glass type",
+				"name",
+				"quit",
+			},
+		}
 
-			if query != "quit" {
-				handleQuery(searchType, query)
+		_, searchValueResult, searchValueErr := searchTypeprompt.Run()
+
+		if searchValueErr != nil {
+			fmt.Printf("Prompt failed %v\n", searchValueErr)
+			return
+		}
+
+		switch {
+		case searchValueResult == "quit":
+			break Main
+		case searchValueResult == "random":
+			handleCocktailSearch(searchValueResult, "")
+		case searchValueResult == "name":
+			valResult, valErr := handleTextSearch(searchValueResult)
+
+			if valErr != nil {
+				fmt.Printf("Something went wrong, please try again")
 			}
-		}
-		if searchType == "quit" || query == "quit" {
-			fmt.Println("Goodbye")
-			break
+			bodyBytes := handleCocktailSearch(searchValueResult, valResult)
+			buildDrinkInstructions(bodyBytes)
 
+		case searchValueResult == "ingredient name":
+			valResult, valErr := handleTextSearch(searchValueResult)
+
+			if valErr != nil {
+				fmt.Printf("Something went wrong, please try again")
+			}
+			bodyBytes := handleCocktailSearch(searchValueResult, valResult)
+			drinkName := buildDrinkList(bodyBytes)
+
+			b := handleCocktailSearch("name", strings.ReplaceAll(drinkName, " ", "_"))
+			buildDrinkInstructions(b)
+		default:
+			fmt.Println("search by ", searchValueResult)
 		}
 	}
 }
 
-func handleQueryType(selection string) string {
-	var response string
-	switch {
-	case strings.Contains("name", selection) || selection == "name":
-		response = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s="
-	case strings.Contains("ing", selection) || selection == "ingredient name":
-		response = "https://www.thecocktaildb.com/api/json/v1/1/search.php?i="
-	case strings.Contains("ran", selection) || selection == "random":
-		response = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
-	case strings.Contains("gla", selection) || selection == "glass type":
-		response = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?g="
-	default:
-		fmt.Println("Unrecognized query, search for cocktail by name")
-		response = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s="
+func handleTextSearch(searchValueResult string) (string, error) {
+	searchValueprompt := promptui.Prompt{
+		Label: "Search:",
 	}
-	return response
+
+	fmt.Printf("Search by: %q\n", searchValueResult)
+	result, err := searchValueprompt.Run()
+
+	if err != nil {
+		return "", errors.New("Something went wrong, please try again")
+	}
+	return result, nil
 }
 
-func handleQuery(searchType, query string) {
-	var fullURL string
-	baseURL := handleQueryType(searchType)
-	if query == "random" {
-		fullURL = baseURL
-	} else {
-	fullURL = baseURL + query
-	}
-	resp, err := http.Get(fullURL)
+func handleCocktailSearch(searchType, query string) []byte {
+	baseURL := handleSearchType(searchType)
+
+	resp, err := http.Get(baseURL + query)
 
 	if err != nil {
 		panic(err)
@@ -138,7 +151,57 @@ func handleQuery(searchType, query string) {
 	defer resp.Body.Close()
 
 	bodyBytes, _ := ioutil.ReadAll(resp.Body)
-	buildDrinkInstructions(bodyBytes)
+
+	return bodyBytes
+}
+
+func handleIngredientSearch(selection string) {
+
+}
+
+func handleSearchType(selection string) string {
+	var response string
+	switch {
+	case selection == "name": // ✔
+		response = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s="
+	case selection == "random": // ✔
+		response = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+	case selection == "ingredient name":
+		response = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i="
+	case selection == "glass type":
+		response = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?g="
+	default:
+		fmt.Println("Unrecognized query, search for cocktail by name")
+		response = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s="
+	}
+	return response
+}
+
+func buildDrinkList(bodyBytes []byte) string {
+	var drinkList DrinkList
+	json.Unmarshal(bodyBytes, &drinkList)
+
+	n := len(drinkList.Drinks)
+
+	// display the list of drinks
+	var drinkchoices []string
+
+	for i := 0; i < n; i++ {
+		drinkchoices = append(drinkchoices, drinkList.Drinks[i].StrDrink)
+	}
+
+	searchTypeprompt := promptui.Select{
+		Label: "Select Drink",
+		Items: drinkchoices,
+	}
+
+	_, result, err := searchTypeprompt.Run()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return result
 }
 
 func buildDrinkInstructions(bodyBytes []byte) {
@@ -160,3 +223,12 @@ func buildDrinkInstructions(bodyBytes []byte) {
 		}
 	}
 }
+
+var BBlack string = "\033[1;30m"
+var BRed string = "\033[1;31m"
+var BGreen string = "\033[1;32m"
+var BYellow string = "\033[1;33m"
+var BBlue string = "\033[1;34m"
+var BPurple string = "\033[1;35m"
+var BCyan string = "\033[1;36m"
+var BWhite string = "\033[1;37m"
